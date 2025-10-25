a6b1ebd80fe1af5f2d1311431a6b7947e8401221
shailendrasharma28 <shailendr2003@gmail.com>
2025-10-25 11:21:11 +0530

    Get all messages fromn db api

--- backend/apis/controllers/chatController.js
+++ backend/apis/controllers/chatController.js
@@ -19,6 +19,37 @@ const chatController = {
                 error: error.message
             })
         }
+    },
+    getAllMessages: async (req, res) => {
+        const {limit, page} = req.query;
+        const parsedLimit = limit ? JSON.parse(limit) : 100;
+        const parsedPages = page ? JSON.parse(page) : 1;
+        try {
+            const {count, rows} = await Message.findAndCountAll({
+                attributes: ["id", "sender_id", "content"],
+                limit: parsedLimit,
+                offset: (parsedPages - 1) * parsedLimit
+            })
+            const response = {
+                rows,
+                count,
+                totalPages: Math.ceil(count/parsedLimit),
+                currentPage: parsedPages
+            }
+            res.status(200).json({
+                success: true,
+                message: "All messages retrieved successfully!",
+                data: response
+            })
+        } catch (error) {
+            console.log(error);
+            
+            res.status(200).json({
+                success: false,
+                message: "Error retrieving messages!",
+                error: error.message
+            })
+        }
     }
 };
 
--- backend/apis/routes/chatRoutes.js
+++ backend/apis/routes/chatRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const chatController = require("../controllers/chatController");
 const router = express.Router();
 
-router.post("/new-message", chatController.sendMessage);
+router.post("/message/new", chatController.sendMessage);
+router.get("/message/all", chatController.getAllMessages);
 
 module.exports = router;
